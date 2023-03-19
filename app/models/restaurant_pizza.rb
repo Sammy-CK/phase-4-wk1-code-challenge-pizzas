54736664f13fc7d70608645c1f7e9917a22a9a0e
Sammy-CK <sammycherono2696@gmail.com>
2023-03-19 09:08:18 +0300

    Added price validation between 1 and 30

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,4 +1,5 @@
 class RestaurantPizza < ApplicationRecord
+    validates :price, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 30 }
     belongs_to :restaurant
     belongs_to :pizza
 end
