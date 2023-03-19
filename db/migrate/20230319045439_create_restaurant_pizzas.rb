12c55214d0b78113a5afe3562cd77d278b30b9d1
Sammy-CK <sammycherono2696@gmail.com>
2023-03-19 08:21:30 +0300

    Run migrations adding foreign keys

--- db/migrate/20230319045439_create_restaurant_pizzas.rb
+++ db/migrate/20230319045439_create_restaurant_pizzas.rb
@@ -1,7 +1,8 @@
 class CreateRestaurantPizzas < ActiveRecord::Migration[7.0]
   def change
     create_table :restaurant_pizzas do |t|
-
+      t.integer :restaurant_id, null: false
+      t.integer :pizza_id, null: false
       t.timestamps
     end
   end
--- db/migrate/20230319050522_add_foreign_key_restaurant_pizza.rb
+++
@@ -1,6 +0,0 @@
-class AddForeignKeyRestaurantPizza < ActiveRecord::Migration[7.0]
-  def change
-    add_foreign_key :restaurant_pizzas, :pizzas, column: :pizza_id
-    add_foreign_key :restaurant_pizzas, :restaurants, column: :restaurant_id
-  end
-end
