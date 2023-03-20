86ae42df12808cb4bdb1019c49bc6201bc08ec5f
Sammy-CK <sammycherono2696@gmail.com>
2023-03-20 14:24:30 +0300

    Successfully Tested

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :restaurant_pizzas, only: [:create]
   resources :pizzas, only: [:index]
   resources :restaurants, only: [:show, :index, :destroy]
+  
 end
